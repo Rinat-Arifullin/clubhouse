3a5ab85591b2608a7e9840984adb97f72395ba84
Rinat-Arifullin <arifullin.rinat98@mail.ru>
2021-11-04 17:02:07 +0300

    Паспорт, github авторизация, sequelize

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,31 +8,47 @@ import TwitterStep from "@steps/TwitterStep";
 import ChooseAvatarStep from "@steps/ChooseAvatarStep";
 import EnterPhoneStep from "@steps/EnterPhoneStep";
 import EnterCodeStep from "@steps/EnterCodeStep";
+import GitHubStep from "components/steps/GitHubStep";
 
 interface IStepsComponents {
   [key: number]: NextComponentType;
 }
 
+export interface IUser {
+  id: number;
+  fullname: string;
+  avatarUrl?: string;
+  isActive: number;
+  username: string;
+  phone: string;
+  createdAt: string;
+  updatedAt: string;
+}
+
 const stepsComponent: IStepsComponents = {
   0: WelcomeStep,
-  1: EnterNameStep,
-  2: TwitterStep,
+  1: GitHubStep,
+  // 2: TwitterStep,
+  2: EnterNameStep,
   3: ChooseAvatarStep,
   4: EnterPhoneStep,
   5: EnterCodeStep,
 };
 
-interface IStepsContextProps {
+interface IAuthContextProps {
   onNextStep: () => void;
   step: number;
+  user?: IUser;
+  setUser: (user: IUser) => void;
 }
 
-export const StepsContext = React.createContext<IStepsContextProps>(
-  {} as IStepsContextProps
+export const StepsContext = React.createContext<IAuthContextProps>(
+  {} as IAuthContextProps
 );
 
 const Home: NextPage = () => {
   const [step, setStep] = React.useState<number>(0);
+  const [user, setUser] = React.useState<IUser>();
   const StepComponent = stepsComponent[step];
 
   const onNextStep = () => {
@@ -44,7 +60,7 @@ const Home: NextPage = () => {
       <Head>
         <title>Clubhouse: Drop-in audio chat</title>
       </Head>
-      <StepsContext.Provider value={{ step, onNextStep }}>
+      <StepsContext.Provider value={{ step, onNextStep, user, setUser }}>
         <StepComponent />
       </StepsContext.Provider>
     </div>
